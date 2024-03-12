cca388fc20131a28892c4403b3eaf0fe3c85e5c8
Gianni Henriques <gianni.henriques@epitech.eu>
2024-03-12 21:37:43 +0100

    feat(core): check number of arguments

--- src/Core/Main.cpp
+++ src/Core/Main.cpp
@@ -8,6 +8,14 @@
 #include <iostream>
 #include "../../include/Errors.hpp"
 
-int main(void) {
+int main(int ac, char **av) {
+    try {
+        if (ac != 2)
+            throw FileError("Invalid number of arguments");
+    } catch (const Error& e) {
+        std::cerr << e.getType() << ": " << e.what() << std::endl;
+        std::cerr << "Exiting with status " << e.getStatus() << std::endl;
+        return 84;
+    }
     std::cout << "Hello, Core!" << std::endl;
 }
