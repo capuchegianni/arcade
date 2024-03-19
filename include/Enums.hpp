4c15cfc3394ba94768a8ceb90f69e580d27148e9
ElouanR <elouan.rigomont@epitech.eu>
2024-03-19 20:41:54 +0100

    feat: add hpp file for all enums

---
+++ include/Enums.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** enums
+*/
+
+#ifndef ENUMS_HPP_
+#define ENUMS_HPP_
+
+enum TilesType {
+    EMPTY,
+    WALL,
+    PLAYER_HEAD,
+    PLAYER_BODY,
+    ENEMY,
+    PROJECTILE,
+    ITEM
+};
+
+enum Input {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    ESC,
+    ENTER,
+    SPACE,
+    NONE
+};
+
+#endif /* !ENUMS_HPP_ */
