1af1344cf70606caea5304f580ffd3fb9ff7adb2
Gianni Henriques <gianni.henriques@epitech.eu>
2024-03-20 17:31:08 +0100

    feat(graphics): create a generic color class

--- include/Graphics/IGraphicalModule.hpp
+++ include/Graphics/IGraphicalModule.hpp
@@ -17,3 +17,12 @@ class IGraphicalModule {
         virtual void destroyWindow() = 0;
         virtual std::string getLibraryType() const = 0;
 };
+
+class Color {
+    public:
+        Color(int r = 255, int g = 255, int b = 255, int a = 255) : r(r), g(g), b(b), a(a) {}
+        int r;
+        int g;
+        int b;
+        int a;
+};
