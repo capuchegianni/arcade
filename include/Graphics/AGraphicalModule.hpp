e5cfd0ab6c287baeeacef9c1efa181a61a05bde3
Gianni Henriques <gianni.henriques@epitech.eu>
2024-03-20 22:46:12 +0100

    feat(graphics): added text, shapes and sprite methods definitions

--- include/Graphics/AGraphicalModule.hpp
+++ include/Graphics/AGraphicalModule.hpp
@@ -15,148 +15,48 @@ class AGraphicalModule : public IGraphicalModule {
         virtual ~AGraphicalModule() = default;
 
         // Window methods
-        /**
-         * @brief Create the window
-         * @param name Title of the window
-         * @param size Size of the window (default: 1920x1080)
-        */
         virtual void createWindow(const std::string& name, const std::vector<int>& size = {1920, 1080}) = 0;
-
-        /**
-         * @brief Initialize the window
-         * @return The newly created window
-        */
         virtual void initWindow() = 0;
-
-        /**
-         * @brief Set the window size
-         * @param size Size of the window (default: 1920x1080)
-        */
         virtual void setWindowSize(const std::vector<int>& size = {1920, 1080}) = 0;
-
-        /**
-         * @brief Set the window title
-         * @param title Title of the window
-        */
         virtual void setWindowTitle(const std::string& title) = 0;
-
-        /**
-         * @brief Display the window
-        */
         virtual void displayWindow() = 0;
-
-        /**
-         * @brief Destroy the window
-        */
         virtual void destroyWindow() = 0;
-
-        /**
-         * @brief Clear the window
-         * @param color Color to clear the window with (default: white)
-        */
         virtual void clearWindow(Color color = {255, 255, 255}) = 0;
-
-        /**
-         * @brief Return the window state
-         * @return True if the window is open, false otherwise
-        */
         virtual bool isWindowOpen() = 0;
-
-        /**
-         * @brief Parse keyboard events
-        */
-        virtual void pollEvents() = 0;
-
+        virtual void parseKeyboard() = 0;
 
         // Text methods
-        /**
-         * @brief Display text on the window
-         * @param text Text to display
-         * @param x X position of the text
-         * @param y Y position of the text
-         * @param size Size of the text
-        */
-        virtual void displayText(const std::string& text) = 0;
+        virtual void displayText(const std::string& text, const std::vector<int>& pos = {0, 0}, const int& size = 30, const Color& color = {255, 255, 255}) = 0;
+        virtual std::any initText(const std::string& text) = 0;
+        virtual void setTextColor(const std::any& text, const Color& color) = 0;
+        virtual void setTextFont(const std::any& text, const std::string& path) = 0;
+        virtual void setTextOutline(const std::any& text, const bool& hasOutline = true) = 0;
+        virtual void setTextOutlineThickness(const std::any& text, const int& thickness = 1) = 0;
+        virtual void setTextOutlineColor(const std::any& text, const Color& color) = 0;
+        virtual void setTextFillColor(const std::any& text, const Color& color) = 0;
+        virtual void setTextSize(const std::any& text, const int& size = 30) = 0;
+        virtual void setTextPosition(const std::any& text, const std::vector<int>& pos = {0, 0}) = 0;
+        virtual void setTextOrigin(const std::any& text, const std::vector<int>& origin = {0, 0}) = 0;
 
-        /**
-         * @brief Initialize text
-         * @return std::any
-        */
-        virtual std::any initText() = 0;
+        // Shapes methods
+        virtual void drawRectangle(const std::vector<int>& size = {10, 10}, const std::vector<int>& pos = {0, 0}, const Color& color = {255, 255, 255}) = 0;
+        virtual void setRectangleColor(std::any& rect, const Color& color) = 0;
+        virtual void setRectangleSize(std::any& rect, const std::vector<int>& size) = 0;
+        virtual void setRectanglePosition(std::any& rect, const std::vector<int>& pos) = 0;
+        virtual void drawCircle(const int& rad = 10, const std::vector<int>& pos = {0, 0}, const Color& color = {255, 255, 255}) = 0;
+        virtual void setCircleColor(std::any& circle, const Color& color) = 0;
+        virtual void setCircleRadius(std::any& circle, const int& rad) = 0;
+        virtual void setCirclePosition(std::any& circle, const std::vector<int>& pos) = 0;
 
-        /**
-         * @brief Set text color
-         * @param color Color of the text
-        */
-        virtual void setTextColor(const Color& color) = 0;
+        // Sprite methods
+        virtual void displaySprite(const std::string& path, const std::vector<int>& pos = {0, 0}, const std::vector<float>& scale = {1, 1}, const float& rotation = 0, const std::vector<int>& origin = {0, 0}, const Color& color = {255, 255, 255}) = 0;
+        virtual std::any initSprite(const std::string& path) = 0;
+        virtual void setSpriteTexture(std::any sprite, const std::string& path) = 0;
+        virtual void setSpritePosition(std::any sprite, const std::vector<int>& pos) = 0;
+        virtual void setSpriteScale(std::any sprite, const std::vector<float>& scale) = 0;
+        virtual void setSpriteRotation(std::any sprite, const float& rotation) = 0;
 
-        /**
-         * @brief Set text font
-         * @param path Path to the font
-        */
-        virtual void setTextFont(const std::string& path) = 0;
-
-        /**
-         * @brief Set text outline
-         * @param hasOutline Boolean to set outline
-        */
-        virtual void setTextOutline(const bool& hasOutline) = 0;
-
-        /**
-         * @brief Set text outline thickness
-         * @param thickness Thickness of the outline
-        */
-        virtual void setTextOutlineThickness(const int& thickness) = 0;
-
-        /**
-         * @brief Set text outline color
-         * @param color Color of the outline
-        */
-        virtual void setTextOutlineColor(const Color& color) = 0;
-
-        /**
-         * @brief Set text fill color
-         * @param color Color of the fill
-        */
-        virtual void setTextFillColor(const Color& color) = 0;
-
-        /**
-         * @brief Set text size
-         * @param size Size of the text
-        */
-        virtual void setTextSize(const int& size) = 0;
-
-        /**
-         * @brief Set text position
-         * @param x X position of the text
-         * @param y Y position of the text
-        */
-        virtual void setTextPosition(const int& x, const int& y) = 0;
-
-        /**
-         * @brief Set text origin
-         * @param x X origin of the text
-         * @param y Y origin of the text
-        */
-        virtual void setTextOrigin(const int& x, const int& y) = 0;
-
-        // virtual void drawRectangle(const int& x1, const int& y1, const int& x2, const int& y2) = 0;
-        // virtual void drawCircle(const int& x, const int& y, const int& rad) = 0;
-        // virtual void drawLine(const int& x1, const int& y1, const int& x2, const int& y2) = 0;
-        // virtual void setLineWidth(const int& width) = 0;
-        // virtual void setOutlineColor(const Color& color) = 0;
-        // virtual void setFillColor(const Color& color) = 0;
-        // virtual void setSprite(const std::string& path) = 0;
-        // virtual void setSpriteRect(const int& x, const int& y, const int& w, const int& h) = 0;
-        // virtual void setSpriteScale(const float& x, const float& y) = 0;
-        // virtual void setSpriteRotation(const float& angle) = 0;
-        // virtual void setSpriteColor(const Color& color) = 0;
-        // virtual void setSpritePosition(const int& x, const int& y) = 0;
-        // virtual void setSpriteOrigin(const int& x, const int& y) = 0;
-        // virtual void setSpriteTextureRect(const int& x, const int& y, const int& w, const int& h) = 0;
-        // virtual void setSpriteTexture(const std::string& path) = 0;
-        // virtual void drawSprite() = 0;
-
+        // Misc methods
         std::string getLibraryType() const;
     protected:
         std::string _librabryType;
