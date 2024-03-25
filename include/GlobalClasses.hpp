9b4657aba07b5ef88e742aa6da0283e3542fd62c
Gianni Henriques <gianni.henriques@epitech.eu>
2024-03-25 15:33:07 +0100

    refactor(tiles): put tiles class in a globalClasses hpp

--- include/GlobalClasses.hpp
+++ include/GlobalClasses.hpp
@@ -1,15 +1,14 @@
 /*
 ** EPITECH PROJECT, 2024
-** B-OOP-400-LIL-4-1-arcade-elouan.rigomont
+** arcade
 ** File description:
-** Tiles
+** GlobalClasses
 */
 
-#ifndef TILES_HPP_
-#define TILES_HPP_
+#pragma once
 
-#include "../Libs.hpp"
-#include "../Enums.hpp"
+#include "./Libs.hpp"
+#include "./Enums.hpp"
 
 class Tiles {
     public:
@@ -20,5 +19,3 @@ class Tiles {
     private:
         TilesType _type = EMPTY;
 };
-
-#endif /* !TILES_HPP_ */
