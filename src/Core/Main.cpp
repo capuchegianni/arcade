dc1d4384fb13c297d01d5d3a693a6f5fab292f59
Gianni Henriques <gianni.henriques@epitech.eu>
2024-03-26 17:11:40 +0100

    refactor(graphics): remove every methods too precise for the interface

--- src/Core/Main.cpp
+++ src/Core/Main.cpp
@@ -14,13 +14,12 @@ int main(int ac, char **av) {
         if (ac != 2)
             throw FileError("Invalid number of arguments\nUSAGE: ./arcade path_to_graphical_library.so");
         core.checkFile(av[1]);
-        core.loadLibrary(av[1], "createLib");
-        core.getModule()->createWindow("Arcade", {500, 500});
-        std::cout << core.getModule()->getLibraryType() << " window created" << std::endl;
-        while (core.getModule()->isWindowOpen()) {
-            core.getModule()->clearWindow();
-            core.getModule()->displayWindow();
-            core.getModule()->parseKeyboard();
+        core.loadGraphicalLibrary(av[1], "createLib");
+        core.getGraphicalModule()->createWindow("Arcade", {500, 500});
+        std::cout << core.getGraphicalModule()->getLibraryType() << " window created" << std::endl;
+        while (core.getGraphicalModule()->isWindowOpen()) {
+            core.getGraphicalModule()->displayWindow();
+            core.getGraphicalModule()->parseKeyboard();
         }
     } catch (const Error& e) {
         std::cerr << e.getType() << ": " << e.what() << std::endl;
