33e6e34aa9004ef9b70fb5bbd77035e2670d0866
AugustinPif <augustin.piffeteau@epitech.eu>
2024-03-29 15:57:20 +0100

    feat(ascii): add get and set ascii for colors

---
+++ src/GlobalClasses/Ascii.cpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-LIL-4-1-arcade-elouan.rigomont
+** File description:
+** Ascii
+*/
+
+#include "../../include/GlobalClasses/Ascii.hpp"
+
+ASCII::ASCII(char ascii, Color color) : _ascii(ascii), _color(color) {}
+
+void ASCII::setAscii(char ascii) 
+{
+    _ascii = ascii;
+}
+
+void ASCII::setColor(Color color) 
+{
+    _color = color;
+}
+
+char ASCII::getAscii() const 
+{
+    return _ascii;
+}
+
+Color ASCII::getColor() const 
+{
+    return _color;
+}
